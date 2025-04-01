5efb527308409463ac1b77e49a92dca274982917
titenopenedx <titenopenedx@example.com>
2025-04-02 01:24:51 +0530

    add new changes problems

---
+++ src/editors/data/constants/messages.ts
@@ -0,0 +1,64 @@
+import { defineMessages } from '@edx/frontend-platform/i18n';
+
+export const messages = defineMessages({
+  customSingleSelectTitle: {
+    id: 'authoring.problemeditor.problemtype.singleSelect.title',
+    defaultMessage: 'Single select',
+  },
+  customMultiSelectTitle: {
+    id: 'authoring.problemeditor.problemtype.multiSelect.title',
+    defaultMessage: 'Multi-select',
+  },
+  customDropdownTitle: {
+    id: 'authoring.problemeditor.problemtype.dropdown.title',
+    defaultMessage: 'Dropdown',
+  },
+  customNumericTitle: {
+    id: 'authoring.problemeditor.problemtype.numeric.title',
+    defaultMessage: 'Numerical input',
+  },
+  customTextInputTitle: {
+    id: 'authoring.problemeditor.problemtype.textInput.title',
+    defaultMessage: 'Text input',
+  },
+  customAdvancedBlankTitle: {
+    id: 'authoring.problemeditor.problemtype.blankProblem.title',
+    defaultMessage: 'Blank problem',
+  },
+  customAdvancedCircuitSchematicTitle: {
+    id: 'authoring.problemeditor.problemtype.circuitSchematic.title',
+    defaultMessage: 'Circuit schematic builder',
+  },
+  customAdvancedCircuitSchematicStatus: {
+    id: 'authoring.problemeditor.problemtype.circuitSchematic.status',
+    defaultMessage: 'Not supported',
+  },
+  customAdvancedJsInputTitle: {
+    id: 'authoring.problemeditor.problemtype.jsInput.title',
+    defaultMessage: 'Custom JavaScript display and grading',
+  },
+  customAdvancedCustomGraderTitle: {
+    id: 'authoring.problemeditor.problemtype.customGrader.title',
+    defaultMessage: 'Custom Python-evaluated input',
+  },
+  customAdvancedCustomGraderStatus: {
+    id: 'authoring.problemeditor.problemtype.customGrader.status',
+    defaultMessage: 'Provisional',
+  },
+  customAdvancedImageTitle: {
+    id: 'authoring.problemeditor.problemtype.imageResponse.title',
+    defaultMessage: 'Image mapped input',
+  },
+  customAdvancedImageStatus: {
+    id: 'authoring.problemeditor.problemtype.imageResponse.status',
+    defaultMessage: 'Not supported',
+  },
+  customAdvancedFormulaTitle: {
+    id: 'authoring.problemeditor.problemtype.formulaResponse.title',
+    defaultMessage: 'Math expression input',
+  },
+  customAdvancedHintTitle: {
+    id: 'authoring.problemeditor.problemtype.problemWithHint.title',
+    defaultMessage: 'Problem with adaptive hint',
+  },
+});
