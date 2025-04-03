d087b1bca24c2100cec220d95e3065922b0129fd
titenopenedx <titenopenedx@example.com>
2025-04-04 00:20:27 +0530

    changes in message file and remove unwanted words

--- src/editors/data/constants/messages.ts
+++ src/editors/data/constants/messages.ts
@@ -3,90 +3,90 @@ import { defineMessages } from '@edx/frontend-platform/i18n';
 export const messages = defineMessages({
   customSingleSelectTitle: {
     id: 'authoring.problemeditor.problemtype.singleSelect.title',
-    defaultMessage: 'Single selectYY',
+    defaultMessage: 'Single select',
   },
   customMultiSelectTitle: {
     id: 'authoring.problemeditor.problemtype.multiSelect.title',
-    defaultMessage: 'Multi-selectYY',
+    defaultMessage: 'Multi-select',
   },
   customDropdownTitle: {
     id: 'authoring.problemeditor.problemtype.dropdown.title',
-    defaultMessage: 'DropdownYY',
+    defaultMessage: 'Dropdown',
   },
   customNumericTitle: {
     id: 'authoring.problemeditor.problemtype.numeric.title',
-    defaultMessage: 'Numerical inputYY',
+    defaultMessage: 'Numerical input',
   },
   customTextInputTitle: {
     id: 'authoring.problemeditor.problemtype.textInput.title',
-    defaultMessage: 'Text inputYY',
+    defaultMessage: 'Text input',
   },
   customAdvancedBlankTitle: {
     id: 'authoring.problemeditor.problemtype.blankProblem.title',
-    defaultMessage: 'Blank problemYY',
+    defaultMessage: 'Blank problem',
   },
   customAdvancedCircuitSchematicTitle: {
     id: 'authoring.problemeditor.problemtype.circuitSchematic.title',
-    defaultMessage: 'Circuit schematic builderYY',
+    defaultMessage: 'Circuit schematic builder',
   },
   customAdvancedJsInputTitle: {
     id: 'authoring.problemeditor.problemtype.jsInput.title',
-    defaultMessage: 'Custom JavaScript display and gradingYY',
+    defaultMessage: 'Custom JavaScript display and grading',
   },
   customAdvancedCustomGraderTitle: {
     id: 'authoring.problemeditor.problemtype.customGrader.title',
-    defaultMessage: 'Custom Python-evaluated inputYY',
+    defaultMessage: 'Custom Python-evaluated input',
   },
   customAdvancedImageTitle: {
     id: 'authoring.problemeditor.problemtype.imageResponse.title',
-    defaultMessage: 'Image mapped inputYY',
+    defaultMessage: 'Image mapped input',
   },
   customAdvancedFormulaTitle: {
     id: 'authoring.problemeditor.problemtype.formulaResponse.title',
-    defaultMessage: 'Math expression inputYY',
+    defaultMessage: 'Math expression input',
   },
   customAdvancedHintTitle: {
     id: 'authoring.problemeditor.problemtype.problemWithHint.title',
-    defaultMessage: 'Problem with adaptive hintYY',
+    defaultMessage: 'Problem with adaptive hint',
   },
   customSingleSelectPreviewDescription: {
     id: 'authoring.problemeditor.problemtype.singleSelect.previewDescription',
-    defaultMessage: 'Learners must select the correct answer from a list of possible optionsYY.',
+    defaultMessage: 'Learners must select the correct answer from a list of possible options.',
   },
   customMultiSelectPreviewDescription: {
     id: 'authoring.problemeditor.problemtype.multiSelect.previewDescription',
-    defaultMessage: 'Learners must select all correct answers from a listYY of possible options.',
+    defaultMessage: 'Learners must select all correct answers from a list of possible options.',
   },
   customDropdownPreviewDescription: {
     id: 'authoring.problemeditor.problemtype.dropdown.previewDescription',
-    defaultMessage: 'Learners must select the correct answer YYfrom a list of possible options',
+    defaultMessage: 'Learners must select the correct answer from a list of possible options',
   },
   customNumericPreviewDescription: {
     id: 'authoring.problemeditor.problemtype.numeric.previewDescription',
-    defaultMessage: 'Specify one or more correct numeric answYYers, submitted in a response field.',
+    defaultMessage: 'Specify one or more correct numeric answers, submitted in a response field.',
   },
   customTextInputPreviewDescription: {
     id: 'authoring.problemeditor.problemtype.textInput.previewDescription',
-    defaultMessage: 'Specify one or more correct text answerYYs, including numbers and special characters, submitted in a response field.',
+    defaultMessage: 'Specify one or more correct text answers, including numbers and special characters, submitted in a response field.',
   },
   customSingleSelectDescription: {
     id: 'authoring.problemeditor.problemtype.singleSelect.description',
-    defaultMessage: 'Enter your single select answers YYY and select which choices are correct. Learners must choose one correct answer.',
+    defaultMessage: 'Enter your single select answers below and select which choices are correct. Learners must choose one correct answer.',
   },
   customMultiSelectDescription: {
     id: 'authoring.problemeditor.problemtype.multiSelect.description',
-    defaultMessage: 'Enter your multi select answers YY and select which choices are correct. Learners must choose all correct answers.',
+    defaultMessage: 'Enter your multi select answers below and select which choices are correct. Learners must choose all correct answers.',
   },
   customDropdownDescription: {
     id: 'authoring.problemeditor.problemtype.dropdown.description',
-    defaultMessage: 'Enter your dropdown answers beYYow and select which choice is correct. Learners must select one correct answer.',
+    defaultMessage: 'Enter your dropdown answers below and select which choice is correct. Learners must select one correct answer.',
   },
   customNumericDescription: {
     id: 'authoring.problemeditor.problemtype.numeric.description',
-    defaultMessage: 'Enter correct numerical input answYYers below. Learners must enter one correct answer.',
+    defaultMessage: 'Enter correct numerical input answers below. Learners must enter one correct answer.',
   },
   customTextInputDescription: {
     id: 'authoring.problemeditor.problemtype.textInput.description',
-    defaultMessage: 'Enter your text input answers belYYow and select which choices are correct. Learners must enter one correct answer.',
+    defaultMessage: 'Enter your text input answers below and select which choices are correct. Learners must enter one correct answer.',
   },
 });
